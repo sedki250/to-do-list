0919f00a671fa5fe2a3d390b5fb1b68d73e22545
sedki250 <marwanfathi250@gamil.com>
2025-09-25 01:55:57 +0300

    Prepare project for Netlify client-side deploy

--- src/main.ts
+++ src/main.ts
@@ -17,20 +17,9 @@
 // })
 // .catch(err => console.error(err));
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { provideRouter, withHashLocation } from '@angular/router';
-import { routes } from './app/app.routes';
-import { provideFirebaseApp } from '@angular/fire/app';
-import { initializeApp } from 'firebase/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { environment } from './environments/environment';
 
-bootstrapApplication(App, {
-  providers: [
-    provideRouter(routes, withHashLocation()),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideAuth(() => getAuth())
-    // لا تستخدم أي prerender / provideClientHydration
-  ]
-})
-.catch(err => console.error(err));
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
+
